ec302cbb23b8cd3b499c3d99da386c1434e0669f
Chenduoru <chenduoru21@qq.com>
2018-06-03 03:42:05 +0800

    js functions

--- ViewModel/app.js
+++ ViewModel/app.js
@@ -3,12 +3,12 @@ var petname
 var pethunger
 var petclean
 var petlove
-
+var url = "http://127.0.0.1:8000/"
 
 function submitRegister() {
 	$.ajax({
 		type: "POST",
-		url: "http://127.0.0.1:8000/signUp/",
+		url: url + "signUp/",
 		dataType: "JSON",
 		crossDomain: true,
 		data: $('#register').serialize(),
@@ -32,7 +32,7 @@ function submitRegister() {
 function login(){
 	$.ajax({
 		type: "GET",
-		url: "http://127.0.0.1:8000/login/",
+		url: url + "login/",
 		dataType: "JSON",
 		crossDomain: true,
 		data: $('#login').serialize(),
@@ -50,4 +50,175 @@ function login(){
 		}
 	});
 
+}
+
+function getUserStatus(){
+	$.ajax({
+		type: "POST",
+		url: url + "getUserStatus/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"username" : username,
+		},
+		success: function (res) {
+			if(res == "noPet"){
+				petname = 0
+			}else{
+				petname = res.petname
+			}
+
+		}
+	});
+}
+
+
+//宠物部分
+function createPet(){
+	$.ajax({
+		type: "POST",
+		url: url + "createPet/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"username" : username,
+			//"petname" :           //引入宠物名 
+		},
+		success: function (res) {
+			if(res == 'success'){
+				alert("创建宠物成功")
+			}
+			else{
+				
+				alert("create pet error")
+			}
+
+		}
+	});
+}
+
+function getPetStatus(){
+	$.ajax({
+		type: "GET",
+		url: url + "getPetStatus/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"petname" : petname,
+		},
+		success: function (res) {
+			pethunger = res.pet_hunger
+	 		petclean = res.pet_clean
+ 			petlove = res.pet_love
+
+		}
+	});
+}
+
+function showAllPet(){
+	$.ajax({
+		type: "GET",
+		url: url + "showAllPet/",
+		dataType: "JSON",
+		crossDomain: true,
+		//data: 
+		success: function (res) {
+			//列表操作
+
+		}
+	});
+}
+
+function showOnePet(){
+	$.ajax({
+		type: "GET",
+		url: url + "showOnePet/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"petname" : petname,
+		},
+		success: function (res) {
+			//列表操作
+
+		}
+	});
+}
+
+function followFriends(){
+	$.ajax({
+		type: "GET",
+		url: url + "followFriends/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"username" : username,
+			//"friendname" :    //朋友名字获取
+		},
+		success: function (res) {
+			if (res == "success"){
+				alert("FollowSuccess")
+			}else{
+				alert("follow failed")
+			}
+
+		}
+	});
+}
+
+function havePet(){
+	$.ajax({
+		type: "GET",
+		url: url + "havePet/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"username" : username,
+			//"petname" :    //宠物名字获取
+		},
+		success: function (res) {
+			if (res == "success"){
+				alert("Success")
+			}else{
+				alert("Failed")
+			}
+
+		}
+	});
+}
+
+function feedPet(){
+	$.ajax({
+		type: "GET",
+		url: url + "feedPet/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"username" : username,
+			"petname" : petname          //引入宠物名 
+		},
+		success: function (res) {
+			pethunger = res.pethunger
+ 			petlove = res.petlove
+
+		}
+	});
+}
+
+function cleanPet(){
+	$.ajax({
+		type: "GET",
+		url: url + "cleanPet/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: {
+			"username" : username,
+			"petname" : petname          //引入宠物名 
+		},
+		success: function (res) {
+			petclean = res.petclean
+ 			petlove = res.petlove
+
+		}
+	});
 }
