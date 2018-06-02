32e2d65732cc51d320ba42580638cf9b05be1768
Chenduoru <chenduoru21@qq.com>
2018-06-03 01:11:07 +0800

    day

--- ViewModel/app.js
+++ ViewModel/app.js
@@ -1,17 +1,48 @@
+var username
+
 function submitRegister() {
 	$.ajax({
 		type: "POST",
-		url: "http://192.168.1.9:8000/signUp/",
+		url: "http://127.0.0.1:8000/signUp/",
 		dataType: "JSON",
 		crossDomain: true,
 		data: $('#register').serialize(),
-		success: function (result) {
-			console.log(result);
+		success: function (res) {
+			if(res == '0'){
+				alert("重复用户名")
+			}
+			else{
+				//data.username = res.
+				//alert(res.username)
+				//alert(res.username)
+				username = res.username
+				alert("注册成功")
+				window.location.replace("login.html")
+			}
 
-
-		},
-		error: function (result) {
-			console.log(result);
 		}
 	});
+}
+
+function login(){
+	$.ajax({
+		type: "GET",
+		url: "http://127.0.0.1:8000/login/",
+		dataType: "JSON",
+		crossDomain: true,
+		data: $('#login').serialize(),
+		success: function (res) {
+			if(res == '0'){
+				alert("用户名或密码错误")
+			}
+			else{
+				
+				username = res.username
+				alert("登录成功")
+				window.location.replace("index.html")
+			}
+
+		}
+	});
+
 }
